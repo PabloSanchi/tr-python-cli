27a09b1f60220450d8a7c18a1c6778bb81eb9692
Paul <pablo.sanchi.herrera@gmail.com>
2024-01-03 19:11:32 +0100

    feat: add lexer

---
+++ core/lexer.py
@@ -0,0 +1,44 @@
+import os, re
+from sly import Lexer
+
+
+class CCTRLexer(Lexer):
+    tokens =  { 
+        CHAR,
+        ALNUM,
+        ALPHA,
+        BLANK,
+        CNTRL,
+        DIGIT,
+        LOWER,
+        PRINT,
+        PUNCT,
+        RUNE,
+        SPACE,
+        SPECIAL,
+        UPPER,
+    }
+    
+    literals = { '-', '"', '[', ']', ':' }
+
+    ALNUM = r'alnum'
+    ALPHA = r'alpha'
+    BLANK = r'blank'
+    CNTRL = r'cntrl'
+    DIGIT = r'digit'
+    LOWER = r'lower'
+    PRINT = r'print'
+    PUNCT = r'punct'
+    RUNE = r'rune'
+    SPACE = r'space'
+    SPECIAL = r'special'
+    UPPER = r'upper'
+    
+    CHAR = r'[A-Za-z0-9]{1}'
+
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno += t.value.count('\n')
+    
+    def error(self, t):
+        raise RuntimeError('Illegal character %r' % t.value[0])

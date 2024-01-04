ea0f5449d9327e8ccecb3ec46ff1a000f8bf46a7
Paul <pablo.sanchi.herrera@gmail.com>
2024-01-04 01:06:45 +0100

    feat: add all falgs functionality

--- main.py
+++ main.py
@@ -1,46 +1,74 @@
-import os, argparse
+import sys, argparse
 from core.lexer import CCTRLexer
 from core.parser import CCTRParser
 from core.range_mapper import RangeMapper
+from core.ranage_deleter import RangeDeleter
+from core.range_squeezer import RangeSqueezer
+from core.range_pipeline import RangePipeline
+
+from utils.user_reader import execute_reader
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-ds", "--delsque", help="Delete and sequeeze")
-parser.add_argument("-d", "--delete", help="Delete mode")
-parser.add_argument("-s", "--squezee", help="Squeeze mode")
-parser.add_argument("string1")
-parser.add_argument("string2", nargs='*', default="", type=str)
+parser.add_argument("-ds", "--delsque", action='store_true', help="Delete and squeeze")
+parser.add_argument("-d", "--delete", action='store_true', help="Delete mode")
+parser.add_argument("-s", "--squeeze", action='store_true', help="Squeeze mode")
+
+parser.add_argument("string1", nargs='?', default=None)
+parser.add_argument("string2", nargs='*', default=None, type=str)
 
 args = parser.parse_args()
 
 lexer = CCTRLexer()
 parser = CCTRParser()
 
-if args.delsque:
-    print("Delete and squeeze mode")
-elif args.delete:
-    print("Delete mode")
-elif args.squezee:
-    print("Squeeze mode")
-else:
-    
-    arg1 = args.string1
-    arg2 = ''.join(args.string2)
-    
-    if not arg1 or not arg2:
-        raise ValueError("Not enough arguments")
-    
-    try:
+def main():
+
+    if args.delsque or args.delete or args.squeeze:
+        if args.string1 is None:
+            raise ValueError("Not enough arguments. One string is required for this mode.")
+        
+        arg1 = args.string1
+        arg2 = ''.join(args.string2) if args.string2 else None
+        
+        if args.delsque:
+            del_range = parser.parse(lexer.tokenize(arg1))
+            sqz_range = parser.parse(lexer.tokenize(arg2))
+            
+            range_deleter = RangeDeleter(del_range)
+            range_squeezer = RangeSqueezer(sqz_range)
+            
+            range_pipeline = RangePipeline([range_deleter, range_squeezer])
+            
+            execute_reader(range_pipeline)
+            
+        elif args.squeeze:
+            src_range = parser.parse(lexer.tokenize(arg1))
+            range_squeezer = RangeSqueezer(src_range)
+            
+            execute_reader(range_squeezer)
+        elif args.delete:
+            src_range = parser.parse(lexer.tokenize(arg1))
+            range_deleter = RangeDeleter(src_range)
+            
+            execute_reader(range_deleter)       
+    else:
+        
+        arg1 = args.string1
+        arg2 = ''.join(args.string2) if args.string2 else None
+        
+        if not arg1 or not arg2:
+            raise ValueError("Not enough arguments. Two strings are required for this mode.")
+        
         src_range = parser.parse(lexer.tokenize(arg1))
         to_range = parser.parse(lexer.tokenize(arg2))
         range_mapper = RangeMapper(src_range, to_range)
         
-        while True:
-            try:
-                user_input = input()
-                output = range_mapper.execute(user_input)
-                print(output)
-            except:
-                break
+        execute_reader(range_mapper)
+
+
+if __name__ == "__main__":
+    try:
+        main()
     except ValueError as err:
-        os.error(err)
+        sys.exit(1)

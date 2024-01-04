c8367c5e776511936eebca2d4277e75004e78e94
Paul <pablo.sanchi.herrera@gmail.com>
2024-01-04 15:06:39 +0100

    fix: remove double quotes from input

--- tests/parser_test.py
+++ tests/parser_test.py
@@ -33,7 +33,7 @@ class TestStringMethods(unittest.TestCase):
             'exception': False
         },
         {
-            'input': '"[:digit:]"',
+            'input': '[:digit:]',
             'output': ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
             'exception': False
         },
